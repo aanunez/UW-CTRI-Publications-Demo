fdc80bf8f525b489708a2b7abbda6b657a54b740
adam nunez <adam.a.nunez@gmail.com>
2021-12-28 09:37:47 -0600

    Layout complete

--- site.js
+++ site.js
@@ -1,9 +1,12 @@
 let ctri = {
 
+    disabledTextColor: '#6c757d',
+    order: 'asc',
+    
     data: [
         {
             title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
+            journal: "Some Fake Fancy Sounding Journal with a long name",
             topic: "",
             date: "2021-03-0",
             pages: "1444-1499",
@@ -19,56 +22,13 @@ let ctri = {
             link: ""
         },
         {
-            title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
-            author: [
-                ['first','middle','last'],
-                ['first','middle','last'],
-                ['first','middle','last'],
-                "Baker TM"
-            ],
-            link_text: 'Full text',
-            link: "#"
-        },
-        {
-            title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
-            author: [
-                ['first','middle','last'],
-                ['first','middle','last'],
-                ['first','middle','last'],
-                "Baker TM"
-            ],
-            link_text: 'Full text',
-            link: ""
-        },
-        {
-            title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
-            author: [
-                ['first','middle','last'],
-                ['first','middle','last'],
-                ['first','middle','last'],
-                "Baker TM"
-            ],
-            link_text: 'Full text',
-            link: ""
-        },
-        {
-            title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
-            author: [
-                ['first','middle','last'],
-                ['first','middle','last'],
-                ['first','middle','last'],
-                "Baker TM"
-            ],
-            link_text: 'Full text',
-            link: ""
-        },
-        {
-            title: "A long paper title here that just keeps going",
-            journal: "Some Fake Fancy Soudning Journal with a long name",
+            title: "A Different Paper title that is still too long, I mean whats the deal with that?",
+            journal: "Fake Journal",
+            topic: "",
+            date: "2021-03-0",
+            pages: "1444-1499",
+            volume: "31",
+            issue: "14",
             author: [
                 ['first','middle','last'],
                 ['first','middle','last'],
@@ -79,19 +39,55 @@ let ctri = {
             link: ""
         }
     ],
+    
+    dataCols: [
+        {
+            title: "display",
+            data: "display",
+            className: "dataTablesDisplayCol",
+            render: (data, type, row, meta) => {
+                return type === 'filter' ? $(data).text() : data;
+            },
+            visible: true
+        },
+        {
+            title: "Article Title",
+            data: "title",
+            visible: false
+        },
+        {
+            title: "Journal",
+            data: "journal",
+            visible: false
+        },
+        {
+            title: "Topic",
+            data: "topic",
+            visible: false
+        },
+        {
+            title: "Primary Author",
+            data: "author",
+            render: (data, type, row, meta) => {
+                return typeof data[0] == "string" ? data[0] : data[0][2];
+            },
+            visible: false
+        },
+        {
+            title: "Year",
+            data: "date",
+            render: (data, type, row, meta) => {
+                return data.split('-')[0];
+            },
+            visible: false
+        }
+    ],
 
     init: () => {
         
         // Setup Talbe
         $('#mainDataTable').DataTable({
-            columns: [{
-                title: "display",
-                data: "display",
-                className: "dataTablesDisplayCol",
-                render: (data, type, row, meta) => {
-                    return type === 'filter' ? $(data).text() : data;
-                }
-            }],
+            columns: ctri.dataCols,
             data: ctri.generateData(),
             createdRow: (row,data,index) => $(row).addClass('dataTablesRow'),
             sDom: 'ftpi',
@@ -124,9 +120,31 @@ let ctri = {
         
         // On resize we need to be sure the expand buttons don't drift
         $(window).on('resize', () => {
-            $(".expandButton").css('transform',`translate(-40px,${$(".dataTablesRow").first().height()-33}px)`)
-        });
-        $(window).resize();
+            $(".dataTablesRow").each( function() {
+                $(this).find(".expandButton").css('transform',`translate(-40px,${$(this).height()-33}px)`)
+            });
+        }).resize();
+        
+        // Insert sort drop down
+        $("#mainDataTable_filter").after(ctri.generateSortDropDown);
+        $(".dataTablesCustom_sort").on('change', ctri.sort).change();
+        $(".dataTablesCustom_order").on('click', ctri.orderToggle);
+    },
+    
+    sort: (e) => {
+        let selection = $(".dataTablesCustom_sort").val();
+        $(".dataTablesCustom_sort").css('color', selection ? 'black' : ctri.disabledTextColor );
+        let index = ctri.dataCols.map(x => x.data).indexOf( $(e.currentTarget).val() );
+        let table = $('#mainDataTable').DataTable();
+        table.order( [ index > -1 ? index : 0, 'asc' ] ).draw();
+    },
+    
+    orderToggle: () => {
+        ctri.order = ctri.order == "asc" ? "desc" : "asc";
+        let table = $('#mainDataTable').DataTable();
+        table.order( [ table.order()[0][0], ctri.order ] ).draw();
+        $(".dataTablesCustom_order i").removeClass('fa-sort-amount-down fa-sort-amount-up')
+        $(".dataTablesCustom_order i").addClass('fa-sort-amount-' + (ctri.order == "asc" ? 'down' : 'up'))
     },
     
     generateData: () => {
@@ -137,7 +155,7 @@ let ctri = {
                 authors.push(typeof el == "string" ? el : (el[2]||"")+" "+(el[0][0]||"")+(el[1][0]||""));
             });
             let link = el.link ? `<a href="${el.link}">[${el.link_text}]</a>` : "";
-            data.push({
+            data.push($.extend({
                 'display': `
                     <div class="container">
                       <div class="row">
@@ -166,7 +184,7 @@ let ctri = {
                       </div>
                     </div>
                 `
-            });
+            }, el));
         });
         return data;
     },
@@ -181,6 +199,20 @@ let ctri = {
     
     generateExpandButton: () => {
         return '<div class="expandButton">+</div>'
+    },
+    
+    generateSortDropDown: () => {
+        let html = "<option value=''>Sort by...</option>";
+        ctri.dataCols.forEach( (el) => {
+            if ( !el.visible ) {
+                html = `${html}<option value="${el.data}">${el.title}</option>`;
+            }
+        });
+        return `
+            <a class="dataTablesCustom_order">
+                <i class="fas fa-sort-amount-down fa-fw"></i>
+            </a>
+            <select class="dataTablesCustom_sort">${html}</select>`;
     }
 };
 
