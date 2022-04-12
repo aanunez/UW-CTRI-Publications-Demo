8f4bda78cf2192eaebd49f6a6c60caf792a5cc08
Adam <adam.a.nunez@gmail.com>
2022-04-11 19:40:23 -0500

    Update site.js

--- site.js
+++ site.js
@@ -131,7 +131,7 @@ let ctri = {
         
         // Setup expand buttons
         jQuery('#mainDataTable tbody').on('click', '.expandButton', (e) => {
-            let $tr = jQuery(e.currentTarget).parent();
+            let $tr = jQuery(e.currentTarget).parent().parent();
             let row = ctri.table.row($tr);
             if (row.child.isShown()) {
                 row.child.hide();
